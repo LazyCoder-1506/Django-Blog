2820ceebcdcd6acbc8f2efca3a9ca4a47bc45f8e
LazyCoder-1506 <pranayan1506@gmail.com>
2021-06-21 20:15:38 +0530

    base template and partials

--- bloggit/settings.py
+++ bloggit/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'bloggit.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templlate')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
