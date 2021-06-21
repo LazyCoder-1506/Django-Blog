6facb1851b7e5dd9f526c8939d934576d29bb88c
LazyCoder-1506 <pranayan1506@gmail.com>
2021-06-21 20:41:52 +0530

    dummy blogs added in db and rendered

--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from .models import Blog
+
 # Create your views here.
 def index(request):
-  return render(request, 'blogs/index.html')
+  blog_list = Blog.objects.order_by('-pub_date')
+  return render(request, 'blogs/index.html', {'blog_list': blog_list})
