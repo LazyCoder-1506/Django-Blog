eca6d2d05bd5899cd49ea57814d976468e9d2997
LazyCoder-1506 <pranayan1506@gmail.com>
2021-06-21 20:09:25 +0530

    admin setup, migrations done

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Blog
+
+admin.site.register(Blog)
+
+admin.site.site_header = 'Bloggit Admin'
+admin.site.site_title = 'Bloggit Admin Area'
+admin.site.index_title = 'Welcome to the Bloggit admin area'
